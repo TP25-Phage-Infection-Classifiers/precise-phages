6bfb92f9c0708bd9475aa2ab1d1950c3227518f3
SanjaNikolic1 <sanja.nikolic@student.uni-tuebingen.de>
2025-05-05 12:10:25 +0200

    Update

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,15 @@
 import os
 import pandas as pd
 import numpy as np
-from scipy.stats import zscore
+
 
 # === PARAMETER ===
-input_dir = "data/Ceyssens_directional_full_raw_counts.tsv"           # Ordner mit Count-Tabellen (*.tsv)
-output_dir = "output/Ceyssens_cleaned.csv"        # Ergebnisordner für bereinigte Dateien und Bericht
+input_file = r"C:\Users\sanja\OneDrive\Documents\GitHub\precise-phages\data\ceyssens_2014\Ceyssens_directional_full_raw_counts.tsv"           # Ordner mit Count-Tabellen (*.tsv)
+os.makedirs("output", exist_ok=True)
+output_file = "output/Ceyssens_cleaned.csv"        # Ergebnisordner für bereinigte Dateien und Bericht
 normalize = True             # True = Normalisierung aktivieren (log1p)
 
-# === HILFSFUNKTIONEN ===
+# HILFSFUNKTIONEN FÜR ERKENNUNG VON AUSREIßER UND NORMALISIERUNG
 def detect_outliers_iqr(df):
     Q1 = df.quantile(0.25)
     Q3 = df.quantile(0.75)
@@ -21,20 +22,20 @@ def normalize_log1p(df):
     return np.log1p(df)
 
 
-# === DATEN EINLESEN ===
+# DATEN EINLESEN 
 df = pd.read_csv(input_file, sep='\t', index_col=0)
 numeric_df = df.select_dtypes(include=[np.number])
 gene_metadata = df.drop(columns=numeric_df.columns, errors='ignore')
 
-# === AUSREIßER ERKENNEN UND ENTFERNEN ===
+# AUSREIßER ERKENNEN UND ENTFERNEN 
 outliers = detect_outliers_iqr(numeric_df)
 cleaned_df = numeric_df.mask(outliers)
 n_outliers = outliers.sum().sum()
 
-# === NORMALISIERUNG ===
+# NORMALISIERUNG (To be edited eventually for User Story 3)
 normalized_df = normalize_log1p(cleaned_df)
 
-# === ERGEBNIS SPEICHERN ===
+# ERGEBNIS SPEICHERN UND AUSGEBEN ===
 final_df = pd.concat([gene_metadata, normalized_df], axis=1)
 final_df.to_csv(output_file)
 
