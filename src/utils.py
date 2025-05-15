91b27d18264dd989f8b39b42018894cf364a3630
Mia Fischer <mi.fischer@student.uni-tuebingen.de>
2025-05-15 17:11:48 +0200

    added new label undefined

--- src/utils.py
+++ src/utils.py
@@ -36,14 +36,20 @@ def normalize_log1p(df):
     return np.log1p(df)
 
 # ordnet jedem Gen ein Label zu, arbeitet mit den normalisierten, bereinigten Phagengendaten
+# hierfür wird geschaut, in welchem drittel der timeslots der maximale Wert an counts liegt. 
+# liegt dieser im ersten Drittel wird das Gen als 'early' gelabled, liegt er im mittleren Drittel als 'middle' und im hinteren als 'late'
+# Sind die normalisierten Counts alle unter einem Wert von 2.5 wird das Gen als 'undefined gelabled und nicht für das ML verwendet
 def classify_temporal_expression(row, time_cols):
+    max_value = row[time_cols].max()
     max_timepoint_idx = row[time_cols].values.argmax() # Index des höchsten Werts der Zeile
     n_timepoints = len(time_cols) # Anzahl der Werte
-    if max_timepoint_idx < n_timepoints / 3: # wenn Index im ersten Drittel liegt -> early
+    if max_value < 2.5: # durchgehend niedrig exprimierte Gene werden als undefined definiert 
+        return 'undefined'
+    elif max_timepoint_idx < n_timepoints / 3: # wenn Index im ersten Drittel liegt -> early
         return 'early'
     elif max_timepoint_idx < 2 * n_timepoints / 3: # wenn Index im zweiten Drittel liegt -> middle
         return 'middle'
-    else: # wenn Index im dritten Drittel liegt -> late
+    else:
         return 'late'
 
 # === DATEIEN VERARBEITEN ===
